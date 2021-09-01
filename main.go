0147d8b25ee66918a8decdb140b0433d2d7cce24
Prasad Jayashanka <prasadjayashanka@gmail.com>
2021-09-01 14:38:31 +0530

    fix

--- main.go
+++ main.go
@@ -12,8 +12,10 @@ import (
 )
 
 const (
-	START_HOUR int = 00
-	END_HOUR   int = 8
+	START_HOUR            int = 00
+	END_HOUR              int = 8
+	DOWNLOAD_WAIT_SECONDS     = 5 //120
+	TIME_CHECK_SECONDS        = 5 //60
 )
 
 func main() {
@@ -25,7 +27,7 @@ func downloadFiles() {
 
 	//open completed log
 	completedLogName := "./completed_list.json"
-	completeLog, err := os.OpenFile(completedLogName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	completeLog, err := os.OpenFile(completedLogName, os.O_RDWR, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
@@ -40,15 +42,25 @@ func downloadFiles() {
 	itemsList := strings.Split(strings.ReplaceAll(string(listBytes), "\r", "\n"), "\n")
 
 	//read completedList
-	completedListBytes, err := os.ReadFile("./list.txt")
+	completedListBytes, err := os.ReadFile(completedLogName)
 	if err != nil {
 		log.Println("error reading list:", err.Error())
 		os.Exit(1)
 	}
 	completedList := make(map[string]string)
-	_ = json.Unmarshal(completedListBytes, &completedList)
+	err = json.Unmarshal(completedListBytes, &completedList)
+	if err != nil {
+		fmt.Println("Error reading completed list : ", err.Error())
+		return
+	}
 
 	itemMap := make(map[string]string)
+
+	if len(itemsList) == 1 && itemsList[0] == "" {
+		fmt.Println("Empty list... exiting in 10 seconds...")
+		time.Sleep(10 * time.Second)
+		os.Exit(0)
+	}
 
 	for _, value := range itemsList {
 		if _, ok := completedList[value]; !ok {
@@ -63,7 +75,7 @@ func downloadFiles() {
 	}
 
 	//set event log
-	logFileName := "logs-" + nowTime.Format("2006-01-02T15:04:05") + ".txt"
+	logFileName := "./logs/logs-" + nowTime.Format("2006-01-02T15:04:05") + ".txt"
 	eventLog, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
@@ -74,16 +86,32 @@ func downloadFiles() {
 	log.Println("Process started at:", nowTime.Format(time.RFC3339))
 
 	for true {
-		nowTimeHour := time.Now().Hour()
-		if nowTimeHour >= START_HOUR && nowTimeHour < END_HOUR {
-			fmt.Println("GOT ITEMS: ", itemMap)
+		//nowTimeHour := time.Now().Hour()
+		//if nowTimeHour >= START_HOUR && nowTimeHour < END_HOUR {
+		if true {
+			if len(itemMap) == 0 {
+				log.Println("completed all downloads.. exiting @ ", nowTime.Format("2006-01-02T15:04:05"))
+				os.Exit(0)
+			}
+
+			fmt.Println("Pending items: ", itemMap)
 			item := ""
 			for k := range itemMap {
 				item = k
 				break
 			}
 			//do the thing
-			fmt.Println("download the file...")
+			fmt.Println("Downloading item : ", item)
+			cmd := exec.Command("youtube-dl", "-i", item, "-f", "137+140")
+			err = cmd.Run()
+			if err != nil {
+				fmt.Println("Download error: ", err.Error())
+				log.Println("Download error : " + item + " : " + err.Error())
+				//delete from map
+				delete(itemMap, item)
+				waitForNextDownload()
+				continue
+			}
 			//delete from map
 			delete(itemMap, item)
 			//add to completed map
@@ -92,28 +120,27 @@ func downloadFiles() {
 			if err != nil {
 				log.Println("exiting reason @ list marshal : ", err.Error())
 			}
+			//write to completed file
+			_ = completeLog.Truncate(0)
+			_, _ = completeLog.Seek(0, 0)
 			_, err = completeLog.Write(b)
 			if err != nil {
 				log.Println("exiting reason @ list write to log : ", err.Error())
 			}
-			fmt.Println("waiting...")
-			time.Sleep(5 * time.Second)
-			fmt.Println("Proceed to next download...")
+
+			waitForNextDownload()
 		} else {
 			fmt.Println("Waiting till midnight...")
-			time.Sleep(5 * time.Second)
+			time.Sleep(TIME_CHECK_SECONDS * time.Second)
 			continue
 		}
 	}
+}
 
-	fmt.Println(time.Now().Hour(), time.Now().Minute())
-	os.Exit(1)
-	cmd := exec.Command("youtube-dl", "-i", "tZFasLMz7Z0", "-f", "137+140")
-	err = cmd.Run()
-	if err != nil {
-		fmt.Println("ERROR: ", err.Error())
-		os.Exit(0)
-	}
+func waitForNextDownload() {
+	fmt.Println("waiting...")
+	time.Sleep(DOWNLOAD_WAIT_SECONDS * time.Second)
+	fmt.Println("Proceed to next download...\n")
 }
 
 func joinFiles() {
